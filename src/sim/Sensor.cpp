02a5be4f18f1cb19300e4409264b2487b9c65d9c
0xphen <raph.kifen@gmail.com>
2025-08-23 20:18:12 +0200

    Fix breaking tests

--- src/sim/Sensor.cpp
+++ src/sim/Sensor.cpp
@@ -15,20 +15,23 @@ typename Sensor<MeasurementSize, StateSize>::MeasurementVector
 Sensor<MeasurementSize, StateSize>::getMeasurement(
     const Eigen::Matrix<double, StateSize, 1> &true_state) const {
   // Generate a random noise vector with the specified covariance
-  typename Sensor<MeasurementSize, StateSize>::MeasurementVector noise =
+  MeasurementVector noise =
       common::generateCorrelatedNoise<MeasurementSize>(R_);
 
   return H_ * true_state +
          noise; // perfect measurement (Hx) plus the generated noise
-
-  const typename Sensor<MeasurementSize, StateSize>::MeasurementMatrix
-      Sensor<MeasurementSize, StateSize>::getMeasurementMatrix {
-    return H_;
-  }
-
-  const typename Sensor<MeasurementSize, StateSize>::CovarianceMatrix
-      Sensor<MeasurementSize, StateSize>::getCovarianceMatrix {
-    return R_;
-  }
 }
+
+template <int MeasurementSize, int StateSize>
+const typename Sensor<MeasurementSize, StateSize>::MeasurementMatrix&
+    Sensor<MeasurementSize, StateSize>::getMeasurementMatrix() const {
+  return H_;
+}
+
+template <int MeasurementSize, int StateSize>
+const typename Sensor<MeasurementSize, StateSize>::CovarianceMatrix&
+    Sensor<MeasurementSize, StateSize>::getCovarianceMatrix() const {
+  return R_;
+}
+
 } // namespace sensor
